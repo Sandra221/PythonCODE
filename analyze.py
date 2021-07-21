d4e197f2c538cf61af5460ed33a992f91a4d4a25
Sandra Börjesson <87774424+Sandra221@users.noreply.github.com>
2021-07-21 23:53:54 +0200

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -10,7 +10,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set_theme(style="darkgrid")
 
 #python 3.9.5
 #And seaborn library (includes matplotlib) for graphs 
